21edf32c550f3b21f04a651bbf887134ad6d5c94
Jurn Wubben <jurnwubben@gmail.com>
2025-01-08 23:21:50 +0100

    Reorganised everything.

--- dot-config/fish/conf.d/atuin.env.fish
+++
@@ -1,6 +0,0 @@
-source "$HOME/.atuin/bin/env.fish" || curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && source "$HOME/.atuin/bin/env.fish" 
-
-if status is-interactive
-	atuin init fish | source
-end
-.
--- dot-config/fish/conf.d/login-compositor.fish
+++
@@ -1,4 +0,0 @@
-if status is-login && uwsm check may-start
-    exec uwsm start hyprland.desktop
-end
-alias hyprland="uwsm start hyprland.desktop"
