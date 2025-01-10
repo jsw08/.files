4ef525b99188ddc224176f1e7926fcd3c4008183
Jurn Wubben <jurnwubben@gmail.com>
2025-01-10 16:55:47 +0100

    Fixed ssh with login shell hopefully.

--- fish/dot-config/fish/conf.d/login-compositor.fish
+++ fish/dot-config/fish/conf.d/login-compositor.fish
@@ -1,4 +1,4 @@
-if status is-login && uwsm check may-start
+if not set -q SSH_TTY && not set -q SSH_CONNECTION && not set -q SSH_CLIENT && status is-login && uwsm check may-start
     exec uwsm start hyprland.desktop
 end
 alias hyprland="uwsm start hyprland.desktop"
